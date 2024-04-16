018f0d56003419df334d9ce4c78fea6b4389b6e7
Sunnt-enganlng <hieu0606sunny@gmail.com>
2024-04-16 09:48:58 +0700

    truy van query

--- src/app/(user)/menu/index.jsx
+++ src/app/(user)/menu/index.jsx
@@ -1,8 +1,18 @@
 import { FlatList, View } from "react-native";
 import ProductListItem from "../../../components/ProductListItem";
 import products from "../../../../assets/data/products";
+import { useEffect } from "react";
+import { supabase } from "../../../lib/supabase";
 
 export default function MenuScreen() {
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data, error } = await supabase.from("products").select("*");
+      // console.log(error);
+      console.log(data);
+    }
+    fetchProducts();
+  }, []);
   return (
     <FlatList
       data={products}
